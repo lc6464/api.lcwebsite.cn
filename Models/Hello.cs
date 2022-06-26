2182f934b5b6f9318a3745dfd61b73890b8d11e2
lc6464 <64722907+lc6464@users.noreply.github.com>
2022-06-26 13:53:47 +0800

    /Hello 增加版权信息

--- Models/Hello.cs
+++ Models/Hello.cs
@@ -1,10 +1,14 @@
-﻿namespace API.Models;
+﻿using System.Reflection;
+
+namespace API.Models;
 public struct Hello {
+	private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
 	public Hello(HttpContext context) => IP = new(context.Connection.RemoteIpAddress, context.Request.Protocol);
 	public Hello(IHttpConnectionInfo info) => IP = new(info);
 
 	public DateTime Time { get; } = DateTime.Now;
-	public Version Version { get; } = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version!;
+	public Version Version { get; } = assembly.GetName().Version!;
+	public string Copyright { get; } = ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCopyrightAttribute))!).Copyright;
 	public string Text { get; init; } = "Welcome to API site of LC's Website.";
 	public IP IP { get; init; }
 }
