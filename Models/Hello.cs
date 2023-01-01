b4d4e4a3baf9296a0c2f85af7835aea57c068c79
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-01-01 19:10:29 +0800

    修复 DeepSource CS-P1018

--- Models/Hello.cs
+++ Models/Hello.cs
@@ -2,7 +2,7 @@
 
 namespace API.Models;
 public readonly struct Hello {
-	private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
+	private static readonly Assembly assembly = typeof(Hello).Assembly;
 	public Hello(HttpContext context) => IP = new(context.Connection.RemoteIpAddress, context.Request.Protocol);
 
 	public Hello(IHttpConnectionInfo info) => IP = new(info);
