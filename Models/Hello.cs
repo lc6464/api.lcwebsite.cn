b079f2d9f914d29a177575e000220a38a8ba2891
lc6464 <64722907+lc6464@users.noreply.github.com>
2024-04-05 15:25:05 +0800

    Fix DeepSource CS-W1091

--- Models/Hello.cs
+++ Models/Hello.cs
@@ -4,7 +4,7 @@ namespace API.Models;
 public readonly struct Hello(IP ip, UserAgent ua) {
 	private static readonly Assembly assembly = typeof(Hello).Assembly;
 
-	public DateTime Time => DateTime.Now;
+	public DateTime Time => DateTime.UtcNow;
 	public Version Version => assembly.GetName().Version!;
 	public string Copyright => ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCopyrightAttribute))!).Copyright;
 	public string Text { get; init; } = "Welcome to API site of LC's Website.";
